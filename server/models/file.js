5be5fe729e7cd0a794d6c9969f23c4b3046a959d
sejal jaiswal <sejaljaiswal716@gmail.com>
2026-06-18 22:31:31 +0530

    file.js

--- server/models/file.js
+++ server/models/file.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
 const fileSchema = new mongoose.Schema({
-  filename: {
-    type: String,
-    required: true,
-  },
+
   originalName: {
     type: String,
   },
@@ -12,13 +9,9 @@ const fileSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  path: {
-    type: String,
-    required: true,
-  },
-  size: {
-    type: Number,
-  },
+  size: Number,
+
+  mimetype: String,
   uploadedAt: {
     type: Date,
     default: Date.now,
